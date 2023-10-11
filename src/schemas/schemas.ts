93cdab5c953d14b4d74e3c2a9d67fa213cee99e3
vitoriacurty <vitoria.curty@hotmail.com>
2023-10-11 20:32:20 -0300

    auth e credential schemas

---
+++ src/schemas/schemas.ts
@@ -0,0 +1,18 @@
+import joi from "joi";
+import { Credential } from "@prisma/client";
+import { AuthEntity } from "../../protocols";
+
+export const authSchema = joi.object<AuthEntity>({
+  email: joi.string().email().required(),
+  password: joi.string().min(10).required(),
+});
+
+// Define um tipo de entrada para credenciais excluindo o campo "id"
+type CredentialInput = Omit<Credential, "id">
+
+export const credentialSchema = joi.object<CredentialInput>({
+  title: joi.string().required(),
+  url: joi.string().uri().required(),
+  username: joi.string().required(),
+  password: joi.string().required()
+})
