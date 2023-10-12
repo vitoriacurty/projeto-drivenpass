8ead180aa837c5b4665cbf1e822c454592faeff6
vitoriacurty <vitoria.curty@hotmail.com>
2023-10-12 13:23:48 -0300

    credential protocol

--- protocols.ts
+++ protocols.ts
@@ -19,3 +19,21 @@ export type SignUp = {
     email: string;
     password: string;
 }
+
+export type CredentialEntity = {
+    userId: number,
+    url: string,
+    username: string,
+    password: string,
+    title: string
+}
+
+export type CredentialUser = {
+    userId: number
+    url: string,
+    username: string,
+    encryptedPassword: string,
+    title?: string
+}
+
+export type CredentialPost = Partial<CredentialEntity>
