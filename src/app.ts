4926ad11b5f23feb7536179b5e5c17ac978256b9
vitoriacurty <vitoria.curty@hotmail.com>
2023-10-16 13:18:30 -0300

    correção app e server

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,4 @@ app
   .use(authRouter)
   .use(credentialRouter)
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is up and running on port: ${port}`);
-})
-
 export default app;
